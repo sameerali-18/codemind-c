098000012eb34626dcc9ca84a34675e3fcafc7a1
Shaik Sameer Ali <119718648+sameerali-18@users.noreply.github.com>
2022-12-30 20:55:37 +0530

    #include<stdio.h> #include<math.h> int main() {     long long int n,no,n2;     int ld,rev=0,r;     scanf("%lld",&n);     ld=n%1000;     while(ld!=0)     {         r=ld%10;         rev=rev*10+r;         ld=ld/10;     }     no=n/1000;     no=no*1000;     n2=no+rev;     {     printf("%lld",n2);     } }

--- Reverse_Last_3_digits.c
+++ Reverse_Last_3_digits.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 #include<math.h>
-int main(){
-long long int n,no,n2;
-int ld,rev=0,r;
-scanf("%lld",&n);
-ld=n%1000;
-while(ld!=0)
+int main()
 {
-r=ld%10;
-rev=rev*10+r;
-ld=ld/10;
-}
-no=n/1000;
-no=no*1000;
-n2=no+rev;
-{
-printf("%lld",n2);
-}
+    long long int n,no,n2;
+    int ld,rev=0,r;
+    scanf("%lld",&n);
+    ld=n%1000;
+    while(ld!=0)
+    {
+        r=ld%10;
+        rev=rev*10+r;
+        ld=ld/10;
+    }
+    no=n/1000;
+    no=no*1000;
+    n2=no+rev;
+    {
+    printf("%lld",n2);
+    }
 }
 
 
