2f0ae671685dd71020ebf7a30875eb8840acd9e5
Shaik Sameer Ali <119718648+sameerali-18@users.noreply.github.com>
2023-01-28 11:07:09 +0530

    #include<stdio.h> int main() {     int n,sum=0;     scanf("%d",&n);         while(sum!=1&&sum!=7&&sum!=4){         sum=0;         while(n>0){             sum+=((n%10)*(n%10));             n/=10;         }         n=sum;     }     if(sum==1 || sum==7)     {         printf("True");     }     else {         printf("False");     } }

---
+++ happy_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+        while(sum!=1&&sum!=7&&sum!=4){
+        sum=0;
+        while(n>0){
+            sum+=((n%10)*(n%10));
+            n/=10;
+        }
+        n=sum;
+    }
+    if(sum==1 || sum==7)
+    {
+        printf("True");
+    }
+    else {
+        printf("False");
+    }
+}
