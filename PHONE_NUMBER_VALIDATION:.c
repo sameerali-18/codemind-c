a96451b7a53e18da73cb9929fe3d644d7c3aa9b3
Shaik Sameer Ali <119718648+sameerali-18@users.noreply.github.com>
2023-01-28 10:52:54 +0530

    #include<stdio.h> int main() {     long long int n,count=0;     scanf("%lld",&n);     while(n)     {         count++;         n/=10;     }     if(count==10)     {         printf("Valid");     }     else     {         printf("Invalid");     } }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    long long int n,count=0;
+    scanf("%lld",&n);
+    while(n)
+    {
+        count++;
+        n/=10;
+    }
+    if(count==10)
+    {
+        printf("Valid");
+    }
+    else 
+    {
+        printf("Invalid");
+    }
+}
